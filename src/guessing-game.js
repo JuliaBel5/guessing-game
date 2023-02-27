ce932aadf17773fc01253b0cda731e01a6341c9c
JuliaBel5 <juliabel5@gmail.com>
2023-02-27 19:45:15 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor(x) {}
+    currentMin = 0;
+    currentMax = 0;
+    currentValue = 0;
+
 
     setRange(min, max) {
-
+        this.currentMin = min;
+        this.currentMax = max;
     }
 
     guess() {
-
-    }
+       this.currentValue = Math.floor(Math.round(this.currentMax -(this.currentMax - this.currentMin)/2));
+            return this.currentValue
+      }
+    
 
     lower() {
-
-    }
+        this.currentMax = this.currentValue
+        }
+    
+    
 
     greater() {
-
+        this.currentMin = this.currentValue
     }
 }
+
 
 module.exports = GuessingGame;
